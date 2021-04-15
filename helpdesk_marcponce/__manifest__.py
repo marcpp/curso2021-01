934102dcfbd5f2549c67663910f144589027482b
MarcPoncePlaza <marcpp@gmail.com>
2021-04-15 21:32:23 +0000

    Helpdesk MarcPonce

--- helpdesk_marcponce/__manifest__.py
+++ helpdesk_marcponce/__manifest__.py
@@ -1,13 +1,8 @@
 # Copyright 2021 Marc Ponce
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 {
-<<<<<<< HEAD
     'name': 'HelpDesk Marc Ponce',
     'summary': 'HelpDesk Marc Ponce',
-=======
-    'name': 'Helpdesk Marc Ponce',
-    'summary': 'Helpdesk Marc Ponce',
->>>>>>> 34150a5c7c0f8d094296f1517f19a07926f37a84
     'version': '14.0.1.0.0',
     'depends': ['base'],
     'author': 'Marc Ponce Plaza',
